5aab637011d95e662ca7d4071b7b1f951f8c939e
Lol gtfo <sup4tdc@yahoo.de>
2017-01-23 20:27:38 +0100

    Funktioniert nicht wirklich
    Es kommt immer wieder der selbe Fehler. Immer weider fixxe ich ihn.
    Immer wieder wird die Datei zurück gesetzt...

--- app/src/main/java/app/gympet/Main.java
+++ app/src/main/java/app/gympet/Main.java
@@ -1,10 +1,11 @@
-package com.example.domin.gympet;
+package app.gympet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
